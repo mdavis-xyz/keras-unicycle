8863fb2efb37c444d739c0e27f90cd528f41eabf
Matthew Davis <Matthew.Davis.2@team.telstra.com>
2018-11-08 17:28:29 +1100

    demo if weights exist

--- mattAgent.py
+++ mattAgent.py
@@ -11,6 +11,7 @@ from rl.memory import SequentialMemory
 
 from gym.envs.registration import registry, register, make, spec
 import gym_unicycle
+import os.path
 
 register(
     id='MATTENV-v0',
@@ -45,17 +46,22 @@ print(model.summary())
 # even the metrics!
 memory = SequentialMemory(limit=50000, window_length=1)
 policy = BoltzmannQPolicy()
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=100,
                target_model_update=1e-2, policy=policy)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+weights_fname = 'dqn_{}_weights.h5f'.format(ENV_NAME)
+if os.path.isfile(weights_fname):
+    print("Loading weights from before")
+    print("Skipping training")
+    dqn.load_weights(weights_fname)
+else:
+    # Okay, now it's time to learn something! We visualize the training here for show, but this
+    # slows down training quite a lot. You can always safely abort the training prematurely using
+    # Ctrl + C.
+    dqn.fit(env, nb_steps=50000, visualize=False, verbose=2)
 
-# Okay, now it's time to learn something! We visualize the training here for show, but this
-# slows down training quite a lot. You can always safely abort the training prematurely using
-# Ctrl + C.
-dqn.fit(env, nb_steps=50000, visualize=True, verbose=2)
-
-# After training is done, we save the final weights.
-dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+    # After training is done, we save the final weights.
+    dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
 dqn.test(env, nb_episodes=5, visualize=True)
